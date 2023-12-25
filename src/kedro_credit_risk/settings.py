2b0441fac09b3f5142db1ece5f369af97f3c2ba4
Stanislaw Cronberg <stanislawcronberg@gmail.com>
2023-12-25 14:57:08 +0100

    Add custom hook to remove unnecessary warnings

--- src/kedro_credit_risk/settings.py
+++ src/kedro_credit_risk/settings.py
@@ -17,6 +17,10 @@ from pathlib import Path  # noqa: E402
 
 from kedro_viz.integrations.kedro.sqlite_store import SQLiteStore  # noqa: E402
 
+from kedro_credit_risk.hooks import ProjectHooks  # noqa: E402
+
+HOOKS = (ProjectHooks(),)
+
 SESSION_STORE_CLASS = SQLiteStore
 # Keyword arguments to pass to the `SESSION_STORE_CLASS` constructor.
 SESSION_STORE_ARGS = {"path": str(Path(__file__).parents[2])}
@@ -30,12 +34,12 @@ from kedro.config import OmegaConfigLoader  # noqa: E402
 CONFIG_LOADER_CLASS = OmegaConfigLoader
 # Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 CONFIG_LOADER_ARGS = {
-      "base_env": "base",
-      "default_run_env": "local",
-#       "config_patterns": {
-#           "spark" : ["spark*/"],
-#           "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
-#       }
+    "base_env": "base",
+    "default_run_env": "local",
+    #       "config_patterns": {
+    #           "spark" : ["spark*/"],
+    #           "parameters": ["parameters*", "parameters*/**", "**/parameters*"],
+    #       }
 }
 
 # Class that manages Kedro's library components.
