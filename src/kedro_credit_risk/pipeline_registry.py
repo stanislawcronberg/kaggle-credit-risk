9f0a60db3bbee5f267a8c5dd185699b87dbe82f2
Stanislaw Cronberg <stanislawcronberg@gmail.com>
2024-03-03 16:45:56 +0100

    update pipelines and remove xgboost

--- src/kedro_credit_risk/pipeline_registry.py
+++ src/kedro_credit_risk/pipeline_registry.py
@@ -1,15 +1,17 @@
-"""Project pipelines."""
-
-from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
+
+from kedro_credit_risk.pipelines import data_processing, feature_engineering_lr, skorecard_modeling
 
 
 def register_pipelines() -> dict[str, Pipeline]:
-    """Register the project's pipelines.
+    pipelines = {
+        "dp": data_processing.create_pipeline(),
+        "fe_lr": feature_engineering_lr.create_pipeline(),
+        "skorecard": data_processing.create_pipeline()
+        + feature_engineering_lr.create_pipeline()
+        + skorecard_modeling.create_pipeline(),
+        "skorecard_modeling": skorecard_modeling.create_pipeline(),
+        "__default__": data_processing.create_pipeline() + feature_engineering_lr.create_pipeline(),
+    }
 
-    Returns:
-        A mapping from pipeline names to ``Pipeline`` objects.
-    """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
     return pipelines
